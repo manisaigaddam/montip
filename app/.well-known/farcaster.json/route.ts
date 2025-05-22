acf1165698aaa1c5e15d2cdbe0324aeeb9894098
manisaigaddam <68413467+manisaigaddam@users.noreply.github.com>
2025-05-23 00:29:20 +0530

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,6 +3,13 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
+    
+  "accountAssociation": {
+    "header": "eyJmaWQiOjI3MjI5MSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweGQ5MzA2ZEM4YmQ3RTk2NzI0OGIxZGY4YjA4Qzk1QTQwY0I3M2JGRjkifQ",
+    "payload": "eyJkb21haW4iOiJtb250aXAudmVyY2VsLmFwcCJ9",
+    "signature": "MHgzZDdmMzk1YzRhYzc5Y2Y5ZGM4YzIzMzBlYTYyMDkzMGRkOGE2NTBjNDliYjUwODQ4ZTMzMzZmNTMwYjgyYmUzMzFlZTM4ODZmY2U4NWYyZjIyYTVmNWMxOGQ3Y2E3ODdhN2YzMzkxMDVhZGNmNDFlNzljNDRhZGNjM2Q2NzVjZDFj"
+  },
+
     // TODO: Add account association
     frame: {
       version: "1",
